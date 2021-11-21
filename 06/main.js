823653f05939356c925435d0cf5721253c57806f
Vladimir Shits <rav9aa@gmail.com>
2021-11-21 23:01:48 +0300

    JS lections 06, comleted tasks to 6a

--- 06/main.js
+++ 06/main.js
@@ -65,42 +65,52 @@ let copyStudents;
 copyStudents = [...students]
 //Проверка:
 console.log('Task 3')
- console.log(copyStudents === students) //- что должно быть в консоли?
- console.log(copyStudents[0] === students[0]) //- что должно быть в консоли?
+console.log(copyStudents === students) //- что должно быть в консоли?
+console.log(copyStudents[0] === students[0]) //- что должно быть в консоли?
 
 //4*. Полная (глубокая) копия массива students (map)
 let deepCopyStudents;
 deepCopyStudents = students.map(i => ({...i}))
 //Проверка:
 console.log('Task 4')
- console.log(deepCopyStudents === students) //- что должно быть в консоли?
- console.log(deepCopyStudents[0] === students[0]) //- что должно быть в консоли?
+console.log(deepCopyStudents === students) //- что должно быть в консоли?
+console.log(deepCopyStudents[0] === students[0]) //- что должно быть в консоли?
 
 // NB!!! Далее все преобразования выполняем не модифицируя исходный массив students
 // Вывод результатов - в консоль
 
 //5. Отсортируйте deepCopyStudents по алфавиту (sort)
 let sortByName;
+sortByName = [...deepCopyStudents].sort((a, b) => a.name > b.name ? 1 : -1)
+console.log('Task 5')
 console.log(sortByName);
 
 //5a. Отсортируйте deepCopyStudents по успеваемости(лучший идёт первым)(sort)
 let sortByScores;
+sortByScores = [...deepCopyStudents].sort((a, b) => a.scores > b.scores ? -1 : 1)
+console.log('Task 5a')
 console.log(sortByScores);
 
 //6. Сформируйте массив студентов, у которых 100 и более баллов (filter)
 let bestStudents;
+bestStudents = students.filter(s => s.scores >= 100)
+console.log('Task 6')
 console.log(bestStudents)
 
 //6a. Получите массив ("вырежьте") из трёх лучших студентов из массива deepCopyStudents (splice)
 //https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
 
 let topStudents;
+topStudents = [...deepCopyStudents].sort((a, b) => a.scores > b.scores ? -1 : 1).splice(0,3)
+console.log('Task 6a')
 console.log(topStudents)
 console.log(deepCopyStudents)
 
 //6b. Объедините массивы deepCopyStudents и topStudents так,
 // чтоб сохранился порядок сортировки (spread-оператор)
 let newDeepCopyStudents;
+
+console.log('Task 6b')
 console.log(newDeepCopyStudents)
 
 //7. Сформируйте массив холостых студентов (filter)
